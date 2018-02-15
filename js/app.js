a6563332051fc331d1ccae155ecaf5bf4a44015f
Pawel Budzynski <budzynski.pawel@gmail.com>
2018-02-15 17:50:10 +0100

    right menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   // alert("DOMContentLoaded");
+  //hamburger
+  var hamburger = document.getElementById('hamburger');
+  var rightMenu = document.getElementById('right-menu');
+  hamburger.addEventListener("click", function onClick(event) {
+    hamburger.classList.toggle('hamburger-active');
+    rightMenu.classList.toggle('move-left');
+
+  });
+  //eo hamburger
+
+
 })
