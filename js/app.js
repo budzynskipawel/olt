3a9b40846cbf840723c739370e2f240062a2fd22
Pawel Budzynski <budzynski.pawel@gmail.com>
2018-02-16 12:10:15 +0100

    right menu

--- js/app.js
+++ js/app.js
@@ -3,12 +3,26 @@ document.addEventListener("DOMContentLoaded", function() {
   //hamburger
   var hamburger = document.getElementById('hamburger');
   var rightMenu = document.getElementById('right-menu');
-  hamburger.addEventListener("click", function onClick(event) {
+  var rightMenuLinks = rightMenu.getElementsByTagName('a');
+  console.log(rightMenuLinks);
+
+  function showHideMenu() {
     hamburger.classList.toggle('hamburger-active');
     rightMenu.classList.toggle('move-left');
-
-  });
+  };
+  hamburger.addEventListener("click", showHideMenu);
+  //close right menu after clicking on link;
+  for(var i = 0; i < rightMenuLinks.length; i++) {
+    rightMenuLinks[i].addEventListener("click", showHideMenu);
+  };
   //eo hamburger
 
 
-})
+
+
+
+
+
+
+
+});
