63b66a70100f91a764fe862a065c345ca324be0c
Pawel Budzynski <budzynski.pawel@gmail.com>
2018-07-06 16:07:34 +0200

    textarea validation

--- js/form.js
+++ js/form.js
@@ -14,6 +14,8 @@
     submitBtn.prop("disabled", true)
   }
 
+
+
   $(function() {
     var wiadomosc = $('#wiadomosc'); //textarea
     var imie = $('#imie'); //input[type="text"]
@@ -95,16 +97,17 @@
       for(var i = 0; i < a.length; i++) {
         if($(a[i]).val() === '') {
           $(a[i]).addClass('empty');
-
-          // event.preventDefault();
+          var offset = $(a[i]).offset();
+          $('html, body').scrollTop(offset.top);
           console.log(a[i], 'zły');
-
+          event.preventDefault();
 
           break;
 
         } else if((i === a.length - 1) && !($(a[i]).val() === '')) {
           testName(imie, nazwisko);
           testAddress(email);
+          testTextarea(wiadomosc);
         } else {
           $(a[i]).removeClass('empty');
           console.log(a[i], 'dobry');
@@ -135,7 +138,9 @@
 
     var namePattern = /^[a-ząęćśółźż-]{3,20}$/i;
     // var emailPattern = /^[a-ząęćśółźż-]{3,20}$/i;
-    var emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    var emailPattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+    var textPattern = /^([0-9A-Za-zĄąĆćĘęŁłÓóŃńŚśŹźŻź,.!?'"();:/-])\w/;
+
 
     function testName() {
       console.log(arguments);
@@ -144,7 +149,8 @@
         if(!namePattern.test(toTest)) {
           $(arguments[i]).addClass('invalid');
           console.log(toTest, 'źle');
-
+          var offset = $(arguments[i]).offset();
+          $('html, body').scrollTop(offset.top);
           event.preventDefault();
           break;
         }
@@ -160,13 +166,30 @@
         if(!emailPattern.test(toTest)) {
           $(arguments[i]).addClass('invalid');
           console.log(toTest, 'źle');
-
+          var offset = $(arguments[i]).offset();
+          $('html, body').scrollTop(offset.top);
           event.preventDefault();
           break;
         }
       }
 
 
+    };
+
+    function testTextarea() {
+      console.log(arguments);
+      for(var i = 0; i < arguments.length; i++) {
+        var toTest = $(arguments[i]).val().replace(/\s+/g, '');
+        console.log(toTest);
+        if(!textPattern.test(toTest)) {
+          $(arguments[i]).addClass('invalid');
+          console.log(toTest, 'źle');
+          var offset = $(arguments[i]).offset();
+          $('html, body').scrollTop(offset.top);
+          event.preventDefault();
+          break;
+        }
+      }
     };
 
 
